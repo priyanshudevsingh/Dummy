cf95deb6035bf8909e227015175ed585834e3516
Priyanshu Singh <therealgrindin.001@gmail.com>
2024-10-31 01:14:39 +0530

    cicd updated

--- server/index.js
+++ server/index.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 
 const port = 3001;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Priyanshu's Server is running on http://localhost:${port}`);
 })
